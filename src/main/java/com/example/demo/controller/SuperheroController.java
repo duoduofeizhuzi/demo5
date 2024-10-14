edfd6b085f81068419ffa9e1d5917c1327703ed0
duoduofeizhuzi <duoduofeizhuzi@gmail.com>
2024-10-15 05:02:03 +0800

    新增捕捉输入非json格式异常（时间关系先不做拦截器，有需要的话我再做）

--- src/main/java/com/example/demo/controller/SuperheroController.java
+++ src/main/java/com/example/demo/controller/SuperheroController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Superhero;
 import com.example.demo.service.SuperheroService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +19,17 @@ public class SuperheroController {
     private SuperheroService service;
 
     @PostMapping
-    public ResponseEntity<String> createHero(@RequestBody Superhero hero) {
+    public ResponseEntity<String> createHero(@RequestBody String rawHeroData) {
+        String validJson = rawHeroData.replace("'", "\"");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        Superhero hero = null;
+        try {
+            hero = objectMapper.readValue(validJson, Superhero.class);
+        } catch (JsonProcessingException e) {
+            return new ResponseEntity<>("Wrong JSON format", HttpStatus.BAD_REQUEST);
+        }
+
         service.addHero(hero);
         return new ResponseEntity<>("Superhero created!", HttpStatus.CREATED);
     }
